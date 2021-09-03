295b6ae68ef925bd72c2bfdb9c8701835b2c71ac
shermanchen <shermanchen@tencent.com>
2021-09-03 23:30:26 +0800

    feat(main): remove environment limit

--- index.js
+++ index.js
@@ -2,40 +2,38 @@
  * polyfill script for ios hippy live-reload on development environment
  */
 
-if (['development', 'dev'].indexOf(process.env.NODE_ENV) >= 0) {
-  (function () {
-    function initLiveReload() {
-      if (!global.WebSocket) return console.warn('websocket is unavailable for current hippy version');
-      const wsUrl = 'ws://127.0.0.1:38999/debugger-live-reload';
-      const ws = new global.WebSocket(wsUrl);
-      ws.onmessage = (evt) => {
-        console.info('[websocket onMessage]', evt);
-        try {
-          const data = JSON.parse(evt.data);
-          if (data.action === 'compileStart') {
-            console.info('[prepare reload bundle]');
-          } else if (data.action === 'compileSuccess') {
-            console.info('[start reload bundle]');
-            ws.close();
-            global.Hippy.bridge.callNative('DevMenu', 'reload');
-          }
-        } catch (err) {
-          console.error('live debug ws onmessage error', err);
-        }
-      };
-      global.Hippy.on('destroyInstance', () => {
-        ws.close();
-      });
-    }
-    if (!global.IS_LIVE_RELOAD_INIT && Hippy.device.platform.OS === 'ios') {
-      // initial only once
-      global.IS_LIVE_RELOAD_INIT = true;
+(function () {
+  function initLiveReload() {
+    if (!global.WebSocket) return console.warn('websocket is unavailable for current hippy version');
+    const wsUrl = 'ws://127.0.0.1:38999/debugger-live-reload';
+    const ws = new global.WebSocket(wsUrl);
+    ws.onmessage = (evt) => {
+      console.info('[websocket onMessage]', evt);
       try {
-        initLiveReload();
-        console.info('[Live Reload Start...]');
-      } catch (e) {
-        console.error('Hippy initLiveReload error', e);
+        const data = JSON.parse(evt.data);
+        if (data.action === 'compileStart') {
+          console.info('[prepare reload bundle]');
+        } else if (data.action === 'compileSuccess') {
+          console.info('[start reload bundle]');
+          ws.close();
+          global.Hippy.bridge.callNative('DevMenu', 'reload');
+        }
+      } catch (err) {
+        console.error('live debug ws onmessage error', err);
       }
+    };
+    global.Hippy.on('destroyInstance', () => {
+      ws.close();
+    });
+  }
+  if (!global.IS_LIVE_RELOAD_INIT && Hippy.device.platform.OS === 'ios') {
+    // initial only once
+    global.IS_LIVE_RELOAD_INIT = true;
+    try {
+      initLiveReload();
+      console.info('[Live Reload Start...]');
+    } catch (e) {
+      console.error('Hippy initLiveReload error', e);
     }
-  }());
-}
+  }
+}());
